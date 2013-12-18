328596a5b624f15babde308ac599e0ab162437da
Nilesh Kumar <nileshkumar.be@gmail.com>
2013-12-18 19:06:06 +0530

    setting up new project for assets management

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetsManagement::Application.config.secret_token = 'a7a610f516788818e974214592a41a86cd7b13e4f25881a5e4fb5f7cb2eee828eb482509667bca0b79651398f795d10761be75dcf96859c3cb366e5a75ad17ea'
