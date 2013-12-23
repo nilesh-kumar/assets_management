f2457c4d8f634008cfd8522d639b138fcb636467
Sapna Prajapati <sapnap@Sapna-Prajapati.(none)>
2013-12-23 12:15:57 +0530

    Resolved bug undefined method persisted for sign in functionality

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,8 +6,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         session[:user_id] = @user.id
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-#        sign_in_and_redirect @user, :event => :authentication
-         redirect_to root_url
+        # sign_in_and_redirect @user, :event => :authentication
+        # redirect_to root_url
+        sign_in_and_redirect root_url, :event => :authentication
       else
         session["devise.google_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
